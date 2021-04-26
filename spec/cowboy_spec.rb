05579014b76e7dd8120add2cbd9f5110bd790001
GideonBrimleaf <chrisdryden1@gmail.com>
2021-04-26 09:05:28 +0100

    linted rspec files

--- spec/cowboy_spec.rb
+++ spec/cowboy_spec.rb
@@ -3,8 +3,8 @@
 require_relative '../src/cowboy'
 
 describe Cowboy do
-  it 'should have a name and a weapon' do
-    wyatt = Cowboy.new('Wyatt Earp', 'Colt Peacemaker')
+  it 'has a name and a weapon' do
+    wyatt = described_class.new('Wyatt Earp', 'Colt Peacemaker')
     expect(wyatt.name).to eql('Wyatt Earp')
     expect(wyatt.weapon).to eql('Colt Peacemaker')
   end
--- src/cowboy.rb
+++ src/cowboy.rb
@@ -1,7 +1,6 @@
 # typed: strict
 # frozen_string_literal: true
 
-# Does a cowboy
 class Cowboy
   extend T::Sig
 
